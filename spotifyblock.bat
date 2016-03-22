402a7d1f756ba2ffd0ff495fe0fc41609f794805
Luuk Grefte <Luuk_Grefte@hotmail.com>
2016-03-22 21:56:52 +0100

    update files

--- spotifyblock.bat
+++ spotifyblock.bat
@@ -1,2 +1,5 @@
- 
-ECHO #Spotify block\n0.0.0.0 pubads.g.doubleclick.net\n0.0.0.0 securepubads.g.doubleclick.net\n>>"C:\Windows\System32\drivers\etc\hosts"
+@ECHO OFF
+echo #Spotify block>>"C:\Windows\System32\drivers\etc\hosts"
+echo 0.0.0.0 pubads.g.doubleclick.net >>"C:\Windows\System32\drivers\etc\hosts"
+echo 0.0.0.0 securepubads.g.doubleclick.net>>"C:\Windows\System32\drivers\etc\hosts"
+pause
--- spotifyblock.sh
+++ spotifyblock.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sudo sed -i '1i #Spotify block\n0.0.0.0 pubads.g.doubleclick.net\n0.0.0.0 securepubads.g.doubleclick.net\n' /etc/hosts
