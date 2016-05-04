6b2f121683a0d9b669c72d5a8120c2afc0cee726
Luuk Grefte <Luuk_Grefte@hotmail.com>
2016-05-04 20:58:47 +0200

    updated url blocks

--- spotifyblock.sh
+++ spotifyblock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo sed -i '1i #Spotify block\n0.0.0.0 pubads.g.doubleclick.net\n0.0.0.0 securepubads.g.doubleclick.net\n' /etc/hosts
+sudo sed -i '1i #Spotify block\n0.0.0.0 pubads.g.doubleclick.net\n0.0.0.0 securepubads.g.doubleclick.net\n0.0.0.0 gads.pubmatic.com\n0.0.0.0 ads.pubmatic.com\n' /etc/hosts
