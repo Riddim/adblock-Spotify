e914d5e24e509a64f42dd6201745ee4d14938489
Luuk Grefte <Luuk_Grefte@hotmail.com>
2016-05-04 21:07:14 +0200

    added block for popup

--- spotifyblock.bat
+++ spotifyblock.bat
@@ -4,4 +4,5 @@ echo 0.0.0.0 pubads.g.doubleclick.net >>"C:\Windows\System32\drivers\etc\hosts"
 echo 0.0.0.0 securepubads.g.doubleclick.net>>"C:\Windows\System32\drivers\etc\hosts"
 echo 0.0.0.0 gads.pubmatic.com>>"C:\Windows\System32\drivers\etc\hosts"
 echo 0.0.0.0 ads.pubmatic.com>>"C:\Windows\System32\drivers\etc\hosts"
+echo 0.0.0.0 partner.googleadservices.com>>"C:\Windows\System32\drivers\etc\hosts"
 pause
