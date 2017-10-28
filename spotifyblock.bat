f27e70babddeba028a0f30a6b006a003be8cd756
Luukgr <Riddim@users.noreply.github.com>
2017-10-28 16:11:57 +0200

    update bat

--- spotifyblock.bat
+++ spotifyblock.bat
@@ -1,8 +1,4 @@
 @ECHO OFF
-echo #Spotify block>>"C:\Windows\System32\drivers\etc\hosts"
-echo 0.0.0.0 pubads.g.doubleclick.net >>"C:\Windows\System32\drivers\etc\hosts"
-echo 0.0.0.0 securepubads.g.doubleclick.net>>"C:\Windows\System32\drivers\etc\hosts"
-echo 0.0.0.0 gads.pubmatic.com>>"C:\Windows\System32\drivers\etc\hosts"
-echo 0.0.0.0 ads.pubmatic.com>>"C:\Windows\System32\drivers\etc\hosts"
-echo 0.0.0.0 partner.googleadservices.com>>"C:\Windows\System32\drivers\etc\hosts"
+set mypath=%cd%
+xcopy /s "mypath\hosts" "C:\Windows\System32\drivers\etc\hosts" /y
 pause
